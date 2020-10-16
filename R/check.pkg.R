c58ca8e2d9e6cf274d3c3c21287000058c6d442e
huhuaping <huhuaping01@nwsuaf.edu.cn>
2020-10-16 22:01:50 +0800

    scrape spider

---
+++ R/check.pkg.R
@@ -0,0 +1,8 @@
+# check.packages function: install and load multiple R packages.
+# Check to see if packages are installed. Install them if they are not, then load them into the R session.
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
---
+++ R/set-global.R
@@ -0,0 +1,39 @@
+
+# global options for R
+options(
+  htmltools.dir.version = FALSE, 
+  formatR.indent = 2, width = 55, 
+  digits = 2,scipen=999, tinytex.verbose = TRUE,
+  knitr.kable.NA = '',
+  echo=FALSE, warning=FALSE, message=FALSE,comment="")
+
+# global options for knitr
+knitr::opts_chunk$set(fig.align='center',echo = FALSE,message = FALSE,warning = FALSE,comment="",
+                      fig.width=11, fig.height=6) # Places figures on their own pages
+
+## set big mark
+### see web [ref](https://stackoverflow.com/questions/30888631/knitr-displaying-digits-of-an-integer-without-scientific-notation)
+
+inline_hook <- function(x) {
+  if (is.numeric(x)) {
+    format(x, digits = 2, big.mark = " ")
+  } else x
+}
+knitr::knit_hooks$set(inline = inline_hook)
+
+
+# global options for DT
+options(DT.options = list(dom ="t" ,  # pure table with no search blank
+                          columnDefs = list(
+                            list(className = "dt-center", targets = "_all"), # align center
+                            list(visible=FALSE,targets=0) # hide index column
+                            )
+                          )
+        )
+
+# global options for servr pkg
+
+options(servr.interval = 0.5) # control time to refresh the preview
+options(servr.daemon = TRUE) # unlock thread when infinite moon render
+
+
